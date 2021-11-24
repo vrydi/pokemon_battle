fa878ab1f49cc5625a5b642653e2a4e5557c37c8
vrydi <57484019+vrydi@users.noreply.github.com>
2021-11-24 19:47:39 +0100

    added toast after generating a new team

--- src/components/pokemon.js
+++ src/components/pokemon.js
@@ -1,19 +1,45 @@
-import {Button, Col, Container, Row} from "react-bootstrap";
+import {Button, Col, Container, Row, Spinner, Toast, ToastContainer} from "react-bootstrap";
 import {PokemonCard} from "./generalComponents/Cards";
 import {usePokemonTeamContext} from "../contexts/PokemonTeamContext";
+import {useState} from "react";
 
 export function PokemonSection() {
-    const {tries} = usePokemonTeamContext()
     return <Container fluid className={'px-5 mt-3'}>
-        {tries > 0 && <PokemonButton/>}
-        <PokemonCards/>
+        <fieldset className={'border border-3 px-4 py-2'} style={{borderRadius: '25px'}}>
+            <div className={'d-flex justify-content-around'}>
+                <h1>My Team</h1>
+                <PokemonButton/>
+            </div>
+            <PokemonCards/>
+        </fieldset>
     </Container>
 }
 
 function PokemonButton () {
-    const {generateTeam} = usePokemonTeamContext()
+    const {generateTeam, tries} = usePokemonTeamContext()
+    const [message, setMessage] = useState('Generate your team')
+    const [showToast, setShowToast] = useState(false)
+
+    const clickButton = () => {
+        setMessage('Generating...')
+        generateTeam().then(()=>{
+            setMessage('Generate your team')
+            setShowToast(true)
+        })
+    }
+
     return <div className={'text-center'}>
-        <Button className={'m-2'} onClick={()=>generateTeam()}>Generate your team</Button>
+        {tries >= 0 && <Button className={'m-2'} onClick={()=>clickButton()}>{message === 'Generating...' ? <Spinner animation="border" size={'sm'}/> : ''}{message}</Button>}
+        {showToast &&
+            <ToastContainer position={'top-center'} className={'mt-5'} style={{zIndex: '10000'}}>
+                <Toast onClose={() => setShowToast(false)} show={showToast} delay={5000} autohide animation={true}>
+                    <Toast.Header>
+                        <strong className="me-auto">Successfully generated a new team</strong>
+                    </Toast.Header>
+                    <Toast.Body>{`${tries +1} generation attempt(s) remaining`}</Toast.Body>
+                </Toast>
+            </ToastContainer>
+        }
     </div>
 }
 
