1dff25a4ad7dc9312d4cb242f9fdd08ea6854598
vrydi <57484019+vrydi@users.noreply.github.com>
2021-11-25 17:38:50 +0100

    added react router

--- src/App.js
+++ src/App.js
@@ -4,22 +4,38 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {PokemonSection} from "./components/pokemon";
 import {PokemonTeamProvider} from "./contexts/PokemonTeamContext";
 import {EnemyPokemonTeamProvider} from "./contexts/EnemyPokemonTeam";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
-
-function ProvidedApp() {
+function DefaultComp(props) {
     return <div className="App">
         <Navigation/>
-        <PokemonSection/>
+        {props.children}
     </div>;
+}
+
+function ProvidedApp() {
+    return <Switch>
+            <Route path={'battle'}>
+                <DefaultComp>
+                </DefaultComp>
+            </Route>
+        <Route path={'/'}>
+            <DefaultComp>
+                <PokemonSection/>
+            </DefaultComp>
+        </Route>
+        </Switch>
 }
 
 function App() {
   return (
-    <PokemonTeamProvider>
-        <EnemyPokemonTeamProvider>
-            <ProvidedApp/>
-        </EnemyPokemonTeamProvider>
-    </PokemonTeamProvider>
+    <Router>
+        <PokemonTeamProvider>
+            <EnemyPokemonTeamProvider>
+                <ProvidedApp/>
+            </EnemyPokemonTeamProvider>
+        </PokemonTeamProvider>
+    </Router>
   );
 }
 
